e50188c5ab58d2ea2a37405fac48e6b9c84f40e8
Nikolaos Vosios <nickbosnv@gmail.com>
2021-07-14 14:35:06 +0300

    Close unlockScreen to MainActivity.

--- app/src/main/java/com/visual/dfls/MainActivity.java
+++ app/src/main/java/com/visual/dfls/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             requestCameraPermission();
         }
 
-        unlockScreen();
+        //unlockScreen();
 
         initializeView();
     }
